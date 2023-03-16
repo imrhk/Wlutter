8763e2a72f514bd9c88ddb56f0d91db5b68ca906
Rahul Kumar <rahul.cs.mnnit@gmail.com>
2023-03-16 23:23:07 +0530

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Wlutter
+
 Flutter Widgets
---
+++ lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Widgets',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: const HomePage(title: 'Flutter Widgets'),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  final String title;
+  const HomePage({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+    );
+  }
+}
